86fa7b00e18f955e7775771d12de96e325833c94
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-11-11 17:17:12 +0100

    auto install dependencies

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ try {
     console.log('Continuing with existing files...\n');
 }
 
+try {
+    console.log('Installing dependencies...');
+    execSync('npm install', { stdio: 'inherit' });
+    console.log('Dependencies installed!\n');
+} catch (error) {
+    console.log('Error installing dependencies');
+    console.error(error.message);
+}
+
 console.log('Starting bot...');
 require('./src/index.js');
