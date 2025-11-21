c1059bbbb8e57ff7622778ac28696f9d7a792e87
uitgeteld <swendewit07@gmail.com>
2025-11-21 11:31:04 +0100

    updated ping command testing

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -1,11 +1,12 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, MessageFlags, Client } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!');
-
-export async function execute(interaction: ChatInputCommandInteraction, client: Client) {
-    const start = Date.now();
-    await interaction.reply({ content: 'Pinging... 🏓', withResponse: true });
-    await interaction.editReply(`Pong! 🏓 Latency: ${Date.now() - start}ms. API: ${client.ws.ping}ms`);
-}
+export default {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction: ChatInputCommandInteraction, client: Client) {
+        const start = Date.now();
+        await interaction.reply({ content: 'Pinging... 🏓', withResponse: true });
+        await interaction.editReply(`Pong! 🏓 Latency: ${Date.now() - start}ms. API: ${client.ws.ping}ms`);
+    }
+};
