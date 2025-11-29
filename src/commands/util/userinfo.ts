816cfe953c7dbd0f915937411aea3a4c62ed6c82
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-11-29 15:24:30 -0800

    Create userinfo.ts

---
+++ src/commands/util/userinfo.ts
@@ -0,0 +1,36 @@
+
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder } from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Replies with user information')
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('The user to get information about')
+                .setRequired(false)),
+    async execute(interaction: ChatInputCommandInteraction, client: Client) {
+        await interaction.deferReply();
+        const user = interaction.options.getUser('target') || interaction.user;
+        const member = interaction.guild?.members.cache.get(user.id);
+        const fullUser = await client.users.fetch(user.id, { force: true });
+        
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ forceStatic: false }) })
+            .setTitle(user.displayName || user.username)
+            .setThumbnail(user.displayAvatarURL({ forceStatic: false }))
+            .addFields(
+                { name: 'Username', value: user.username, inline: true },
+                { name: 'User ID', value: user.id, inline: true },
+                { name: 'Account Created', value: `<t:${Math.floor(user.createdTimestamp / 1000)}:F>`, inline: false },
+                { name: 'Joined Server', value: member ? `<t:${Math.floor(member.joinedTimestamp! / 1000)}:F>` : 'N/A', inline: false },
+            )
+            .setColor('#C9C2B2');
+
+        if (fullUser.banner) {
+            embed.setImage(fullUser.bannerURL({ size: 1024 })!);
+        }
+
+        await interaction.editReply({ embeds: [embed] });
+    }
+};
