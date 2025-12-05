aa17e023c7f15d340eb9b6675544f1fde1293382
uitgeteld <swendewit07@gmail.com>
2025-12-05 12:16:41 +0100

    Handling the events in directories

--- src/functions/handleEvents.ts
+++ src/functions/handleEvents.ts
@@ -4,19 +4,38 @@ import path from "node:path";
 import { loadModule } from "../helpers/loadModule";
 
 export default async function handleEvents(client: Client, eventsPath: string) {
-  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".ts") || file.endsWith(".js"));
-  
-  for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = await loadModule(filePath);
-    const evt = event.default || event;
+  const eventEntries = fs.readdirSync(eventsPath);
 
-    if (evt.once) {
-      console.log(`Registering one-time event: ${evt.name} | ${file}`);
-      client.once(evt.name, (...args: any[]) => evt.execute(...args, client));
-    } else {
-      console.log(`Registering recurring event: ${evt.name} | ${file}`);
-      client.on(evt.name, (...args: any[]) => evt.execute(...args, client));
+  for (const entry of eventEntries) {
+    const entryPath = path.join(eventsPath, entry);
+    const stats = fs.statSync(entryPath);
+    
+    if (stats.isDirectory()) {
+      const eventFiles = fs.readdirSync(entryPath).filter(file => file.endsWith(".ts") || file.endsWith(".js"));
+      for (const file of eventFiles) {
+        const filePath = path.join(entryPath, file);
+        const event = await loadModule(filePath);
+        const evt = event.default || event;
+
+        if (evt.once) {
+          console.log(`Registering one-time event: ${evt.name} | ${entry}/${file}`);
+          client.once(evt.name, (...args: any[]) => evt.execute(...args, client));
+        } else {
+          console.log(`Registering recurring event: ${evt.name} | ${entry}/${file}`);
+          client.on(evt.name, (...args: any[]) => evt.execute(...args, client));
+        }
+      }
+    } else if (entry.endsWith(".ts") || entry.endsWith(".js")) {
+      const event = await loadModule(entryPath);
+      const evt = event.default || event;
+
+      if (evt.once) {
+        console.log(`Registering one-time event: ${evt.name} | ${entry}`);
+        client.once(evt.name, (...args: any[]) => evt.execute(...args, client));
+      } else {
+        console.log(`Registering recurring event: ${evt.name} | ${entry}`);
+        client.on(evt.name, (...args: any[]) => evt.execute(...args, client));
+      }
     }
   }
 }
