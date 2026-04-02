97ffa72c43e6be65dc6548af7ee83d1dc53909c1
uitgeteld <swendewit07@gmail.com>
2026-04-02 11:44:38 +0200

    Spotify -> nowplaying

--- src/commands/util/dev.ts
+++ src/commands/util/dev.ts
@@ -1,7 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder, AttachmentBuilder } from "discord.js";
-import { MusicCard } from "../../functions/image";
-import { isPlayingSpotify } from "../../utils/spotifyPresence";
-
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from "discord.js";
 export default {
     data: new SlashCommandBuilder()
         .setName('dev')
@@ -9,37 +6,5 @@ export default {
     devOnly: true,
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
         await interaction.deferReply();
-
-        const user = interaction.options.getUser('user') || interaction.user;
-        let presence =
-            interaction.guild?.members.cache.get(user.id)?.presence ??
-            client.guilds.cache
-                .map(guild => guild.members.cache.get(user.id))
-                .find(member => member?.presence != null)
-                ?.presence;
-
-        if (isPlayingSpotify(presence)) {
-            const spotifyActivity = isPlayingSpotify(presence);
-            if (spotifyActivity.state == null) return await interaction.editReply('This user is not listening to music on Spotify.');
-            const songStartTime = spotifyActivity.timestamps?.start;
-            const songEndTime = spotifyActivity.timestamps?.end;
-            const songDuration = (songEndTime - songStartTime) / 1000;
-            const elapsedTime = ((Date.now() - songStartTime) / 1000) < 0 ? 0 : (Date.now() - songStartTime) / 1000;
-
-            const card = await new MusicCard()
-                .setSong(spotifyActivity.details)
-                .setArtist(spotifyActivity.state)
-                .setAlbum(spotifyActivity.assets.largeText)
-                .setCover(`https://i.scdn.co/image/${spotifyActivity.assets.largeImage.slice(8)}`)
-                .setTime(elapsedTime, songDuration)
-                .build()
-
-            const attachment = new AttachmentBuilder(card!, { name: 'musicCard.png' });
-            interaction.editReply({
-                files: [attachment]
-            });
-        } else {
-            await interaction.editReply('This user is not currently in a server where I am present.');
-        }
     }
 };
---
+++ src/commands/util/nowplaying.ts
@@ -0,0 +1,52 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client, AttachmentBuilder } from "discord.js";
+import { isPlayingSpotify } from '../../utils/spotifyPresence';
+import { MusicCard } from '../../functions/image/index'
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('nowplaying')
+        .setDescription('Replies with a Music Card')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to check music status for')
+                .setRequired(false)),
+    aliases: ['np', 'spotify'],
+    async execute(interaction: ChatInputCommandInteraction, client: Client) {
+        await interaction.deferReply();
+
+        const user = interaction.options.getUser('user') || interaction.user;
+        let presence =
+            interaction.guild?.members.cache.get(user.id)?.presence ??
+            client.guilds.cache
+                .map(guild => guild.members.cache.get(user.id))
+                .find(member => member?.presence != null)
+                ?.presence;
+
+        if(!presence) return await interaction.editReply('This user is currently not in a server where I am present.');
+
+        if (isPlayingSpotify(presence)) {
+            const spotifyActivity = isPlayingSpotify(presence);
+
+            if (spotifyActivity.state == null) return await interaction.editReply('This user is currently not listening to music.');
+
+            const songStartTime = spotifyActivity.timestamps?.start;
+            const songEndTime = spotifyActivity.timestamps?.end;
+            const songDuration = (songEndTime - songStartTime) / 1000;
+            const elapsedTime = ((Date.now() - songStartTime) / 1000) < 0 ? 0 : (Date.now() - songStartTime) / 1000;
+
+            const card = await new MusicCard()
+                .setSong(spotifyActivity.details)
+                .setArtist(spotifyActivity.state)
+                .setAlbum(spotifyActivity.assets.largeText)
+                .setCover(`https://i.scdn.co/image/${spotifyActivity.assets.largeImage.slice(8)}`)
+                .setTime(elapsedTime, songDuration)
+                .build()
+
+            const attachment = new AttachmentBuilder(card!, { name: 'musicCard.png' });
+
+            interaction.editReply({ files: [attachment] });
+        } else {
+            await interaction.editReply('This user is currently not listening to music.');
+        }
+    }
+};
