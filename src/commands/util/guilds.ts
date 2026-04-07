748e0b6244e81d09824b76aae7b64fb39fc485c3
uitgeteld <swendewit07@gmail.com>
2026-04-07 10:58:39 +0200

    Guilds command update

--- src/commands/util/guilds.ts
+++ src/commands/util/guilds.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client, EmbedBuilder, MessageFlags } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
@@ -6,13 +6,33 @@ export default {
         .setDescription('Replies with list of guilds the bot is in'),
     devOnly: true,
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
-        await interaction.deferReply();
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral});
 
-        const guilds = client.guilds.cache.map(guild => guild.name).join('\n');
+        const guildLines: string[] = [];
+
+        for (const guild of client.guilds.cache.values()) {
+            let guildDisplay = guild.name;
+
+            try {
+                const invites = await guild.invites.fetch();
+                if (invites.size > 0) {
+                    const validInvite = invites.find(inv => !inv.channel || inv.channel.type !== 2);
+                    if (validInvite) {
+                        guildDisplay = `${guild.name} [➜](https://discord.gg/${validInvite.code})`;
+                    }
+                }
+            } catch (error) {
+                // No permission or invite fetch failed
+            }
+
+            guildLines.push(guildDisplay);
+        }
+
+        const guildsText = guildLines.join('\n');
 
         const embed = new EmbedBuilder()
-            .setTitle('Guilds')
-            .setDescription(guilds || 'The bot is not in any guilds.')
+            .setTitle(`Guilds (${client.guilds.cache.size})`)
+            .setDescription(guildsText || 'The bot is not in any guilds.')
             .setColor('#C9C2B2');
 
         await interaction.editReply({ embeds: [embed] });
